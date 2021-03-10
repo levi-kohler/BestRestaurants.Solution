911bc4945d6a58349c24a59c2b671a76d4a5baf1
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-10 16:02:28 -0600

    add details route
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>

--- BestRestaurants/Controllers/CuisinesController.cs
+++ BestRestaurants/Controllers/CuisinesController.cs
@@ -33,5 +33,11 @@ namespace BestRestaurants.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+      return View(thisCuisine);
+    }
   }
 }
