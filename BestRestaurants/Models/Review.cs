20e72e78ab7730af866ce0f2bdf3c7958fceb8cc
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-11 14:52:32 -0600

    add UserRating property and corresponing form input
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>

--- BestRestaurants/Models/Review.cs
+++ BestRestaurants/Models/Review.cs
@@ -5,7 +5,16 @@ namespace BestRestaurants.Models
     public int ReviewId { get; set; }
     public string Name { get; set; }
     public string UserReview { get; set; }
+    public UserRating UserRating { get; set; }
     public int RestaurantId { get; set; }
     public virtual Restaurant Restaurant { get; set; }
+  }
+    public enum UserRating
+  {
+    One,
+    Two,
+    Three,
+    Four,
+    Five
   }
 }
